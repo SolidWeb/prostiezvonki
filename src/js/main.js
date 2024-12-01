b82d266ab4c8a462392c7ab9d5ceaa497a423257
SolidWeb <Grandmonde@ya.ru>
2024-12-01 21:45:46 +0300

    wip: Support page

--- src/js/main.js
+++ src/js/main.js
@@ -224,6 +224,48 @@ checkDialogSupport();
 initDialog();
 
 /**
+  Accordion
+========= **/
+const accordions = document.querySelectorAll('.accordion');
+
+const accordion = () => {
+  accordions.forEach((accordion) => {
+    const buttons = accordion.querySelectorAll('.accordion-button');
+    buttons.forEach((button) => {
+      const isExpanded = button.getAttribute('aria-expanded');
+      isExpanded || button.setAttribute('aria-expanded', 'false');
+      button.tagName === 'A' && button.setAttribute('role', 'button');
+      button.addEventListener('click', toggleAccordion);
+    });
+  });
+};
+
+function toggleAccordion(e) {
+  e.preventDefault();
+
+  const accordion = this.closest('.accordion');
+  const accordionMultiExpand = accordion.classList.contains('accordion--multi');
+  const isExpanded = this.getAttribute('aria-expanded');
+
+  if (!accordionMultiExpand) {
+    const accordionButtons = accordion.querySelectorAll('.accordion-button');
+    for (let i = 0; i < accordionButtons.length; i++) {
+      accordionButtons[i].setAttribute('aria-expanded', 'false');
+    }
+  }
+
+  if (isExpanded === 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+
+  if (isExpanded === 'true') {
+    this.setAttribute('aria-expanded', 'false');
+  }
+}
+
+accordion();
+
+/**
   Dropdown
 ======== **/
 import Dropdown from './components/dropdown';
